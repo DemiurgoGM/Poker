7b9028895b53596e7338ad651ee5ae0177faad39
DemiurgoGM <44926378+DemiurgoGM@users.noreply.github.com>
2020-03-14 18:17:49 -0300

    Back-end function to check Straight Flushes.

--- Deck.py
+++ Deck.py
@@ -1,4 +1,3 @@
-
 class Card:
     def __init__(self, card_value, suit):
         self.value = card_value
@@ -29,3 +28,60 @@ class Deck:
         for i in self.cards_list:
             string += f"{i}\n"
         return string
+
+
+def getKicker(hand):
+    first_card = hand[0]
+    new_hand = hand[0:1]
+    for card in new_hand:
+        if first_card.value == 1:
+            return 14
+        if card.value > first_card.value or card.value == 1:
+            first_card = card
+    return first_card.value
+
+
+def isStaightFlush(hand):
+    first_card = hand[0]
+    new_hand = hand[1:]
+    counter = 0
+    for card in new_hand:
+        if (card.value == first_card.value + 1 or (card.value == 1 and first_card.value == 13)) \
+                and card.suit == first_card.suit:
+            first_card = card
+            counter = counter + 1
+            continue
+        else:
+            break
+    return counter == 5
+
+
+def compare_hands(hand_one, hand_two):
+    if isStaightFlush(hand_one):
+        if isStaightFlush(hand_two):
+            if getKicker(hand_one) > getKicker(hand_two):
+                return hand_one
+            elif getKicker(hand_one) < getKicker(hand_two):
+                return hand_two
+            else:
+                return list()  # Draw = empty list
+        else:
+            return hand_one
+    elif isStaightFlush(hand_two):
+        return hand_two
+    elif isQuad(hand_one):
+        pass
+    elif isFullHouse(hand_one):
+        pass
+    elif isFlush(hand_one):
+        pass
+    elif isStraight(hand_one):
+        pass
+    elif isThreeEquals(hand_one):
+        pass
+    elif isTwoPair(hand_one):
+        pass
+    elif isPair(hand_one):
+        pass
+    else:  # Bigger kicker wins
+        pass
