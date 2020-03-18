4b76b0c2b3579244fb3936ba9482b56e06d16ae1
DemiurgoGM <44926378+DemiurgoGM@users.noreply.github.com>
2020-03-18 17:12:55 -0300

    Small development on PlayPoker.

--- app.py
+++ app.py
@@ -15,12 +15,17 @@ def hello_world():
 
 @app.route('/PlayPoker.html')
 def play_poker():
-    user = request.args
-    player = Player(user['user'], user['value'])
     deck = Deck()
-    random.shuffle(deck.cards_list)
-    return render_template('PlayPoker.html',
-                           username=player.user, value=player.money, deck=deck)
+    for _ in range(0, 7):
+        random.shuffle(deck.cards_list)
+    if request.args.get('phase') is None:
+        user = request.args
+        player = Player(user['user'], user['value'])
+        return render_template('PlayPoker.html',
+                               username=player.user, value=player.money, deck=deck)
+    else:
+        user = request.args  # expected args: user/value/phase/text/
+        pass
 
 
 if __name__ == '__main__':
